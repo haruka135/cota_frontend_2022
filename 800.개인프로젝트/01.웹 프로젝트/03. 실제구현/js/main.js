5cc1154c848f689f4155de9c7fe7eff7f11711c7
haruka135 <harune135@naver.com>
2023-03-07 03:53:04 +0900

    A
    A

---
+++ 800.개인프로젝트/01.웹 프로젝트/03. 실제구현/js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded",loadFn);
+
+function loadFn() {
+    const ham = document.querySelector(".ham a");
+    const hiddenMenu = document.querySelector(".hidden-menu");
+    const hiddenAll = document.querySelector(".hidden-all");
+}
