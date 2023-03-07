b7365a7c7e3e52d84de69a8a4716a425c5abb01c
haruka135 <harune135@naver.com>
2023-03-08 02:09:48 +0900

    a
    a

--- 800.개인프로젝트/01.웹 프로젝트/03. 실제구현/js/main.js
+++ 800.개인프로젝트/01.웹 프로젝트/03. 실제구현/js/main.js
@@ -1,7 +1,54 @@
 window.addEventListener("DOMContentLoaded",loadFn);
 
 function loadFn() {
-    const ham = document.querySelector(".ham a");
-    const hiddenMenu = document.querySelector(".hidden-menu");
-    const hiddenAll = document.querySelector(".hidden-all");
+
+    const bbtn = document.querySelectorAll(".bbtn");
+    const bslide = document.querySelector(".business-slide");
+    const bl = document.querySelectorAll(".bullet > li");
+
+    let bulist = document.querySelectorAll(".business-slide li");
+
+    bulist.forEach((ele,idx) => {
+        ele.setAttribute("data-seq",idx);
+    });
+
+    const infoSlide = (seq) => {
+        let commonlist = bslide.querySelectorAll("li");
+
+        if(seq) {
+            bslide.style.left= "-100%";
+            bslide.style.transition = "left .4s ease-in-out";
+
+            setTimeout(()=>{
+                bslide.appendChild(commonlist[0]);
+                bslide.style.left = "0";
+                bslide.style.transition = "none";
+            },400);
+        } else {
+            bslide.insertBefore(commonlist[commonlist.length - 1],commonlist[0]);
+            bslide.style.left = "-100%";
+            bslide.style.transition = "none";
+
+            setTimeout(()=>{
+                bslide.style.left = "0";
+                bslide.style.transition = "left .4s ease-in-out";
+            },0);
+        }
+
+        bulist = bslide.querySelectorAll("li");
+
+        let comseq = bulist[seq].getAttribute("data-seq");
+
+        for(let x of bl) x.classList.remove("act");
+
+        bl[comseq].classList.add("act");
+    };
+
+    bbtn.forEach((ele,idx)=>{
+        ele.onclick = ()=>{
+            event.preventDefault();
+            console.log("버튼 눌렀어")
+            infoSlide(idx);
+        };
+    });
 }
