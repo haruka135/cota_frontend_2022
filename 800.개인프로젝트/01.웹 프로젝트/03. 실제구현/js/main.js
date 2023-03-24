939fb88f4758acbcf1bae6687058919dffb3647a
haruka135 <harune135@naver.com>
2023-03-24 17:07:14 +0900

    a
    a

--- 800.개인프로젝트/01.웹 프로젝트/03. 실제구현/js/main.js
+++ 800.개인프로젝트/01.웹 프로젝트/03. 실제구현/js/main.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded",function() {
-    const pslide = document.querySelector(".visual-list");
-    const bullet = document.querySelectorAll(".bar");
-    const plist = document.querySelectorAll(".visual-list > li");
+
+    const ham = document.querySelector(".ham a");
+    const hamspan = document.querySelectorAll(".ham a span");
+    const hmenu = document.querySelector(".hidden-menu");
+
+    ham.onclick = () => {
+        hmenu.classList.toggle("on");
+    }
+
+    let bar_seq = 0;
+    const bar = document.querySelectorAll(".bar");
+    setInterval(()=>{
+        bar[bar_seq].classList.remove("on");
+        bar_seq++;
+        if(bar_seq===3) bar_seq = 0;
+        bar[bar_seq].classList.add("on")
+    },3000)
+
 });
